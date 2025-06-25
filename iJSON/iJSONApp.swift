60baeda77a97ba85ff688655b128dd97db22d116
Siraj Qazi <siraj.qazi@seeloz.com>
2025-06-25 06:33:11 +0500

    Initial Commit

---
+++ iJSON/iJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  iJSONApp.swift
+//  iJSON
+//
+//  Created by blakberrisigma on 25/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct iJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iJSONTests/iJSONTests.swift
@@ -0,0 +1,17 @@
+//
+//  iJSONTests.swift
+//  iJSONTests
+//
+//  Created by blakberrisigma on 25/06/2025.
+//
+
+import Testing
+@testable import iJSON
+
+struct iJSONTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
