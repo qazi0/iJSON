ec5ac34ff702e7a10eb6898cea14ff56006c235c
Siraj Qazi <siraj.qazi@seeloz.com>
2025-06-25 11:14:06 +0500

    feat(iJSON/Notifications.swift): add notification name extensions for various app events

---
+++ iJSON/Notifications.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+extension Notification.Name {
+    static let openJSONFile = Notification.Name("openJSONFile")
+    static let saveJSONFile = Notification.Name("saveJSONFile")
+    static let zoomIn = Notification.Name("zoomIn")
+    static let zoomOut = Notification.Name("zoomOut")
+    static let expandAll = Notification.Name("expandAll")
+    static let collapseAll = Notification.Name("collapseAll")
+    static let showAboutSheet = Notification.Name("showAboutSheet")
+    static let copyOutput = Notification.Name("copyOutput")
+}
