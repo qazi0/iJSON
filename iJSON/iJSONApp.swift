cd25100dc51f8e8b03c2a2198bac6f5dcd753589
Siraj Qazi <siraj.qazi@seeloz.com>
2025-06-25 18:43:47 +0500

    fix(iJSONApp.swift): replace default File menu with new implementation and move View menu to standard placement after Window Arrangement

--- iJSON/iJSONApp.swift
+++ iJSON/iJSONApp.swift
@@ -20,43 +20,38 @@ struct iJSONApp: App {
                 }
             }
             
-            CommandGroup(after: .appInfo) { // Add File menu after App Info
-                Menu("File") {
-                    Button("Open JSON File...") {
-                        NotificationCenter.default.post(name: .openJSONFile, object: nil)
-                    }
-                    .keyboardShortcut("o", modifiers: .command)
-                    
-                    Button("Save JSON File...") {
-                        NotificationCenter.default.post(name: .saveJSONFile, object: nil)
-                    }
-                    .keyboardShortcut("s", modifiers: .command)
+            CommandGroup(replacing: .newItem) { // Replace default File menu
+                Button("Open JSON File...") {
+                    NotificationCenter.default.post(name: .openJSONFile, object: nil)
                 }
+                .keyboardShortcut("o", modifiers: .command)
+                
+                Button("Save JSON File...") {
+                    NotificationCenter.default.post(name: .saveJSONFile, object: nil)
+                }
+                .keyboardShortcut("s", modifiers: .command)
             }
             
-            CommandGroup(after: .newItem) { // Add View menu after New Item (standard placement)
-                Menu("View") {
-                    Button("Zoom In") {
-                        NotificationCenter.default.post(name: .zoomIn, object: nil)
-                    }
-                    .keyboardShortcut("+", modifiers: .command)
-                    
-                    Button("Zoom Out") {
-                        NotificationCenter.default.post(name: .zoomOut, object: nil)
-                    }
-                    .keyboardShortcut("-", modifiers: .command)
-                    
-                    Divider()
-                    
-                    Button("Expand All") {
-                        NotificationCenter.default.post(name: .expandAll, object: nil)
-                    }
-                    Button("Collapse All") {
-                        NotificationCenter.default.post(name: .collapseAll, object: nil)
-                    }
+            CommandGroup(after: .windowArrangement) { // Add View menu after Window Arrangement (standard placement)
+                Button("Zoom In") {
+                    NotificationCenter.default.post(name: .zoomIn, object: nil)
+                }
+                .keyboardShortcut("+", modifiers: .command)
+                
+                Button("Zoom Out") {
+                    NotificationCenter.default.post(name: .zoomOut, object: nil)
+                }
+                .keyboardShortcut("-", modifiers: .command)
+                
+                Divider()
+                
+                Button("Expand All") {
+                    NotificationCenter.default.post(name: .expandAll, object: nil)
+                }
+                Button("Collapse All") {
+                    NotificationCenter.default.post(name: .collapseAll, object: nil)
                 }
             }
-            
         }
     }
 }
